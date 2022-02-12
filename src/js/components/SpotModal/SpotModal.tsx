b2d589a9aa6a9c51f7d79d7250082b995a3e5356
annaliarosed <annaliadestefano@gmail.com>
2022-02-12 11:10:28 -0600

    implement and style modal

--- src/js/components/SpotModal/SpotModal.tsx
+++ src/js/components/SpotModal/SpotModal.tsx
@@ -5,16 +5,44 @@ type ModalDataInformation = {
   image: string;
   distance: string;
   title: string;
+  price: number;
+  description: string;
 };
 
 type SpotModalProps = {
-  data?: ModalDataInformation;
+  spotData: ModalDataInformation;
+  handleClose: () => void;
 };
 
-const SpotModal = ({}: SpotModalProps) => {
+const SpotModal = ({ spotData, handleClose }: SpotModalProps) => {
+  const { title, description, price } = spotData;
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const formattedPrice = formatter.format(price);
+
+  console.log(formattedPrice);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.modal}></div>
+      <div className={styles.modal}>
+        {/* Style X better */}
+        <button onClick={handleClose} className={styles.closeButton}>
+          x
+        </button>
+        <div className={styles.modalContent}>
+          <h1>Spot Details</h1>
+          <span className={styles.contentWrapper}>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <button
+              className={styles.bookButton}
+            >{`${formattedPrice} | Book it!`}</button>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/js/spot/SpotItem.tsx
+++ src/js/spot/SpotItem.tsx
@@ -1,33 +1,25 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useState } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import Image from "../common/Image";
 import TextButton from "../common/TextButton";
 import SpotModal from "../components/SpotModal/SpotModal";
 
-type DataInformation = {
-  image: string;
-  distance: string;
-  title: string;
-};
-
 type SpotItemProps = {
   showDetails: boolean;
   isSelected: boolean;
-  data: DataInformation;
-  onDetailsClick: () => void;
+  //TODO: type this correctly
+  data: any;
 };
 
 // todo
 // handle own details click
 //remove uncessary props for this project: is selected, show details, onDetails click
 
-const SpotItem = ({
-  showDetails = true,
-  isSelected,
-  data,
-  onDetailsClick,
-}: SpotItemProps) => {
+const SpotItem = ({ showDetails = true, isSelected, data }: SpotItemProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  console.log("data", data);
+
   return (
     <div
       className={classNames("SpotItem", {
@@ -40,14 +32,17 @@ const SpotItem = ({
         <p>{data.distance}</p>
         {showDetails && (
           <div>
-            hello
-            {/* @ts-expect-error:sdfg */}
-            <TextButton onClick={onDetailsClick}>hello</TextButton>
+            {/* @ts-expect-error:TODO: fix this typing */}
+            <TextButton onClick={() => setIsModalOpen(true)}>
+              Details
+            </TextButton>
           </div>
         )}
       </div>
 
-      <SpotModal />
+      {isModalOpen && (
+        <SpotModal spotData={data} handleClose={() => setIsModalOpen(false)} />
+      )}
     </div>
   );
 };
